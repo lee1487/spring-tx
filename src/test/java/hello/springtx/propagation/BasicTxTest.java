7095b77b9c69ac0b3b57b0c968bf3a52d74ecbea
hyeonse <dlgustp1487@naver.com>
2022-07-02 18:34:47 +0900

    스프링 트랜잭션 전파7 - REQUIRES_NEW

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -126,13 +126,19 @@ public class BasicTxTest {
     void inner_rollback_requires_new() {
         log.info("외부 트랜잭션 시작");
         TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
-        log.info("outer.isNewTransaction={}", outer.isNewTransaction());
+        log.info("outer.isNewTransaction={}", outer.isNewTransaction()); // true
 
         log.info("내부 트랜잭션 시작");
         DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
         definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
         TransactionStatus inner = txManager.getTransaction(definition);
-        log.info("inner.isNewTransaction={}", inner.isNewTransaction());
+        log.info("inner.isNewTransaction={}", inner.isNewTransaction()); // true
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); // 롤백
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer); // 커밋
     }
 
 }
