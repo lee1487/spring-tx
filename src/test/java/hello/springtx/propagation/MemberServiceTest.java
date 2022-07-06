1f32e7a2dbe0c7522b4bcf65d90aab823f440d84
hyeonse <dlgustp1487@naver.com>
2022-07-06 20:30:30 +0900

    트랜잭션 전파 활용2 - 커밋, 롤백

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -29,8 +30,27 @@ class MemberServiceTest {
         memberService.joinV1(username);
 
         //then
-        Assertions.assertTrue(memberRepository.find(username).isPresent());
-        Assertions.assertTrue(logRepository.find(username).isPresent());
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
     }
+
+    /**
+     * memberService        @Transactional:OFF
+     * memberRepository     @Transactional:ON
+     * logRepository        @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
 
 }
